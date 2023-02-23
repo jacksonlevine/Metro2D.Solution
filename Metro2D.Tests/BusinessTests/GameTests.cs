5a93c550c82b52b14c7b7983f1350d0ec75ce345
jacksonlevine <jack@netbug.us>
2023-02-22 17:25:17 -0800

    Create main game file structure

---
+++ Metro2D.Tests/BusinessTests/GameTests.cs
@@ -0,0 +1,7 @@
+using Metro2D.Business;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Metro2D.Tests 
+{
+  
+}
---
+++ Metro2D/Business/Game.cs
@@ -0,0 +1,5 @@
+using System;
+namespace Metro2D.Business
+{
+  
+}
