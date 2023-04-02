d54697c3e0ab44c985499978750873f1393e10a7
jacksonlevine <jack@netbug.us>
2023-04-02 16:05:51 -0700

    wip: a game plan

--- Metro2D/Business/Game.cs
+++ Metro2D/Business/Game.cs
@@ -1,8 +1,13 @@
 using System;
+using SFML.Graphics;
+using SFML.Window;
+using SFML.System;
+
 namespace Metro2D.Business
 {
   public class GameState
   {
-    
+      public Window window;
+      
   }
 }
