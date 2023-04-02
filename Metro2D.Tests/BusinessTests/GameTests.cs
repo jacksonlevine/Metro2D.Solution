9b8a8783dd44204ea89bf44ab603279644b3fcf4
jacksonlevine <jack@netbug.us>
2023-04-02 15:25:22 -0700

    Write first test for gamestate singleton

--- Metro2D.Tests/BusinessTests/GameTests.cs
+++ Metro2D.Tests/BusinessTests/GameTests.cs
@@ -3,5 +3,14 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Metro2D.Tests 
 {
-  
+  [TestClass]
+  public class GameStateSingletonTests
+  {
+    [TestMethod]
+    public void GameState_CreatesInstanceOf_GameState()
+    {
+      GameState g = new GameState();
+      Assert.AreEqual(typeof(GameState), g.GetType());
+    }
+  }
 }
